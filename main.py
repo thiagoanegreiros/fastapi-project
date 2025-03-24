e25aad8364a36fc79b799589c8bb30f651fe41af
Thiago Ananias <thiagoananias@gmail.com>
2025-03-24 15:19:05 -0300

    favicon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 from api.routes import user_router
 from core.container import Container
@@ -9,8 +11,12 @@ container = Container()
 container.init_resources()
 app.container = container  # Adiciona o container ao FastAPI
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(user_router.router)
 
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.ico")
 
 @app.get("/")
 def read_root():
